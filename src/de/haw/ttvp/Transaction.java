872200b841af2efc06a55c4c0f16d3f35be16d1c
lSoleyl <lSoleyl+github@gmail.com>
2014-12-19 14:31:06 +0100

    Datenstruktur der History angepasst.
     * Einträge werden richtig in die History einsortiert, auch wenn sie out-of-order eintreffen.
     * History gibt entsprechende Meldungen raus, wenn etwas besonderes passiert.

--- src/de/haw/ttvp/Transaction.java
+++ src/de/haw/ttvp/Transaction.java
@@ -14,10 +14,17 @@ public final class Transaction {
     currentID = id;
   }
   
+  
+  public static boolean validIDFrom(int from, int to) {
+    if (!(to >= from || from > (to + Integer.MAX_VALUE/2)))
+      return false;
+    return true;
+  }
+  
   /** Checks whether the given ID has already been seen and a broadcast having this ID should be dropped
    */
   public static boolean validID(int id) {
-    if (!(id >= currentID || currentID > (id + Integer.MAX_VALUE/2))) {
+    if (!validIDFrom(currentID, id)) {
       logger.warn("! Received transaction ID is lower than the last ID. Received ID: (" + 
               id + ")  last ID: (" + currentID + ")");
       return false;
--- src/de/haw/ttvp/gamelogic/History.java
+++ src/de/haw/ttvp/gamelogic/History.java
@@ -1,17 +1,20 @@
 package de.haw.ttvp.gamelogic;
 
-import java.util.ArrayList;
+import de.haw.ttvp.Transaction;
+import de.uniba.wiai.lspi.util.logging.Logger;
+import java.util.LinkedList;
 import java.util.List;
 
 public class History {
-
-	private List<HistoryEntry> entries;
+  private static final Logger log = Logger.getLogger(History.class);
+  
+	private LinkedList<HistoryEntry> entries;
 	
 	public History(){
-		this.entries = new ArrayList<>();
+		this.entries = new LinkedList<>(); //Linked list performanter beim Einfügen.
 	}
 	
-	class HistoryEntry{
+	public class HistoryEntry {
 		public int transactionID;
 		public Player srcPlayer;
 		public Player dstPlayer;
@@ -31,8 +34,29 @@ public class History {
 	}
 	
 	public void addEntry(HistoryEntry entry){
-		this.entries.add(entry);
+    if (entries.isEmpty() || Transaction.validIDFrom(entries.getLast().transactionID, entry.transactionID))
+      entries.add(entry);
+    
+    //ID ist "out-of-order" oder ein Duplikat
+    for(int i = entries.size()-1; i >= 0; --i) {
+      HistoryEntry e = entries.get(i);
+      if (e.transactionID == entry.transactionID) {
+        log.warn("Dropping duplicate broadcast with Transaction-ID: " + e.transactionID);
+        //TODO prüfen, ob Einträge identisch sind und Unterschiede ausgeben
+        return;
+      }
+      if (Transaction.validIDFrom(e.transactionID, entry.transactionID)) {
+        entries.add(i, entry);
+        log.info("Added delayed Entry with Transaction-ID: " + entry.transactionID + ". Arrived out of order");
+        return;
+      }
+    } 
+		
 	}
+  
+  public void addEntry(int transactionID, Player srcPlayer,	Player dstPlayer, int slot, boolean hit) {
+    addEntry(new HistoryEntry(transactionID, srcPlayer, dstPlayer, slot, hit));
+  }
 	
 	public List<HistoryEntry> getEntries(){
 		return this.entries;
